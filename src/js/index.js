32206a0b9f05314973e4f77f2ba88f4fb7bda986
Dragos Sarpe <contact@usdgadget.com>
2018-11-22 13:21:02 +0200

    Added axios and API calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,14 @@
-import str from './models/Search';
-// import { add, multiply, ID } from './views/searchView';
-// import { add as a, multiply as m, ID } from './views/searchView';
-import * as searchView from './views/searchView';
+import axios from 'axios';
 
-// console.log(`Using imported functions! ${add(ID, 2)} and ${multiply(3,5)}. ${str}`);
-// console.log(`Using imported functions! ${a(ID, 2)} and ${m(3,6)}. ${str}`);
-console.log(`Using imported functions! ${searchView.add(searchView.ID, 5)} and ${searchView.multiply(3, 8)}. ${str}`);
+async function getResults(query) {
+    const key = 'df4647627bd4df1caaa22b36efeed70a';
+    try {
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+
+}
+getResults('tomato pasta');
