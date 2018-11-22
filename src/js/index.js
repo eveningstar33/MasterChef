97bd2b1b2af63296060c678cad1805feada781e5
Dragos Sarpe <contact@usdgadget.com>
2018-11-22 11:56:34 +0200

    Added ES6 Modules for MVC pattern

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,8 @@
-// Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Variable x is ${x}`); 
+import str from './models/Search';
+// import { add, multiply, ID } from './views/searchView';
+// import { add as a, multiply as m, ID } from './views/searchView';
+import * as searchView from './views/searchView';
+
+// console.log(`Using imported functions! ${add(ID, 2)} and ${multiply(3,5)}. ${str}`);
+// console.log(`Using imported functions! ${a(ID, 2)} and ${m(3,6)}. ${str}`);
+console.log(`Using imported functions! ${searchView.add(searchView.ID, 5)} and ${searchView.multiply(3, 8)}. ${str}`);
