afd1857215796c7502d0e75422567c239f2389e9
Dragos Sarpe <contact@usdgadget.com>
2018-11-23 15:41:41 +0200

    Rendering an AJAX loading spinner

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,5 +1,31 @@
+/*
+  base is where I put the styles that are reusable and other stuff like these elements, 
+  they're also being used and reused across different modules. 
+*/
+
 export const elements = {
     searchForm: document.querySelector('.search'),
     searchInput: document.querySelector('.search__field'),
+    searchRes: document.querySelector('.results'),
     searchResList: document.querySelector('.results__list')
+};
+
+export const elementStrings = {
+    loader: 'loader'
+};
+
+export const renderLoader = parent => {
+    const loader = `
+        <div class="${elementStrings.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
+    `;
+    parent.insertAdjacentHTML('afterbegin', loader); 
+};
+
+export const clearLoader = () => {
+    const loader = document.querySelector(`.${elementStrings.loader}`);
+    if (loader) loader.parentElement.removeChild(loader); 
 };
