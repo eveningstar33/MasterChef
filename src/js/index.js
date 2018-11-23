e3b4e417373a41ed9bee6e2dcaac6554bb9b1a28
Dragos Sarpe <contact@usdgadget.com>
2018-11-23 19:01:05 +0200

    Added search results pagination

--- src/js/index.js
+++ src/js/index.js
@@ -38,5 +38,21 @@ elements.searchForm.addEventListener('submit', e => {
     controlSearch();
 });
 
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
 
+    /*
+    If I click on the span or on the icon but it will find the closest element with the button inline class.
+    So no matter where I click we only get the button which is exactly where this data-goto is located. 
+    So, what we're interested for is the button where we have stored the page nummber. We really need to
+    just select this exact element and not anny of their child elements
+    */
+    
+    if (btn) {
+        // This is a very good way to having access to data, and 10 is base 10
+        const goToPage = parseInt(btn.dataset.goto, 10); 
+        searchView.clearResults(); 
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
 
