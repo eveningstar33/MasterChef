35d007dabe9c6be7ed25f81d67f52f0d57a9ab13
Dragos Sarpe <contact@usdgadget.com>
2018-11-28 11:12:07 +0200

    Added the Recipe View

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,2 @@
 export const proxy = 'https://cors-anywhere.herokuapp.com/';
-export const key = '462b1cc8d4f2730081462fbc65136320';
-
+export const key = 'cb75451786d424896aa887701ab9d846';
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView'; 
 import { elements, renderLoader, clearLoader } from './views/base';
 import Recipe from './models/Recipe';
 
@@ -75,6 +76,13 @@ const controlRecipe = async () => {
 
     if (id) {
         // Prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe); 
+
+        // Highlight selected search item
+        if (state.search) { 
+            searchView.highlightSelected(id);
+        }
 
         // Create new recipe object
         state.recipe = new Recipe(id);
@@ -83,13 +91,16 @@ const controlRecipe = async () => {
             // Get recipe data and parse ingredients
             await state.recipe.getRecipe();
             state.recipe.parseIngredients();
-            console.log(state.recipe);
+            // console.log(state.recipe);
 
             // Calculate time and servings;
             state.recipe.calcTime();
             state.recipe.calcServings();
 
             // Render recipe
+            clearLoader();
+            recipeView.renderRecipe(state.recipe); 
+
         } catch(err) {
             alert('Error processing recipe!');
         }
